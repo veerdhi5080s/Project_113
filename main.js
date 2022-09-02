a48474ddbbfa488a8fa403c7ec2c4d446be1c28c
veerdhi5080s <93766445+veerdhi5080s@users.noreply.github.com>
2022-09-02 15:56:09 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function preload(){
+
+}
+
+function setup(){
+canvas = createCanvas(350, 350);
+canvas.center();
+}
+
+function draw(){
+
+}
+
+function take_snapshot(){
+    save("my_picture.jpeg");
+}
